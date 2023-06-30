487c874a9b3b3cb90bce8938108f4d04d48ceac4
Marcelina Lima <marcelina.batista@hotmail.com>
2023-06-30 16:06:13 -0300

    add.Pasta jogo_lista- com arquivos dos exercicios

---
+++ calculadora/divisao.py
@@ -0,0 +1,8 @@
+#Crie uma função divisão(), que receba dois números como parâmetros 
+# e retorne o resultado da divisão do primeiro pelo segundo.
+
+def divisao(num1, num2):
+    resultado = num1 / num2
+    return resultado
+resultado_divisao = divisao(10, 5)
+print(resultado_divisao)
---
+++ calculadora/velocidade.py
@@ -0,0 +1,11 @@
+
+#Modifique a função feita em sala velocidade() para que utilize a função divisão 
+#para calcular a velocidade
+
+
+def velocidade(distancia, tempo):
+    v = distancia / tempo
+    return
+    print("velocidade: {} m/s".format(v))
+
+velocidade(100,10)    
