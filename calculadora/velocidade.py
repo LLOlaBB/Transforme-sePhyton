c77c2da77afeed86ec4c7ad17ae8d8e3793a54a7
Marcelina Lima <marcelina.batista@hotmail.com>
2023-07-03 21:08:50 -0300

    Alteraçoes pasta calculadora

--- calculadora/velocidade.py
+++ calculadora/velocidade.py
@@ -2,10 +2,18 @@
 #Modifique a função feita em sala velocidade() para que utilize a função divisão 
 #para calcular a velocidade
 
+def divisao(num1,num2):
+    if num2 != 0:
+        div = num1 / num2
+        return div
+    else:
+        return False
 
-def velocidade(distancia, tempo):
-    v = distancia / tempo
-    return
-    print("velocidade: {} m/s".format(v))
+def velocidade(distancia,tempo):
+    if divisao(distancia,tempo) != False:
+        velocidade = divisao(distancia,tempo)
+        return velocidade
+    else:
+        return 'Não é possível dividir por zero'
 
-velocidade(100,10)    
+print(velocidade(100,10))
